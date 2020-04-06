9c059c31d8af76612075aea4c51a91c188c934c5
IfeoluwaLawal <il693@nyu.edu>
2020-04-05 23:16:19 -0400

    chapter2 exercises

---
+++ chapter2/fiveTimes.py
@@ -0,0 +1,12 @@
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (%s)' % str(i))
+    i = i + 1
+# for i in range(5):
+#     print('Jimmy Five Times (%s)' % str(i))
+
+total = 0
+for num in range(3, 2):
+    total = total + num
+print(total)
